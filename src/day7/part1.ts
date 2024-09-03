cc47e1b604ca98ce977c8ac093afa70bd46a8890
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-09-03 21:51:43 +0530

    src: solved part1

---
+++ src/day7/part1.ts
@@ -0,0 +1,41 @@
+function isTLS(ip: string): boolean {
+  const outsideBrackets = ip.replace(/\[.*?\]/g, "");
+
+  for (let i = 0; i < outsideBrackets.length - 3; i++) {
+    const a = outsideBrackets[i];
+    const b = outsideBrackets[i + 1];
+    const c = outsideBrackets[i + 2];
+    const d = outsideBrackets[i + 3];
+    if (a == d && b == c && a !== b) {
+      return true;
+    }
+  }
+
+  //ABBA check inside square brackets
+  const insideBrackets = ip.match(/\[.*?\]/g);
+  if (insideBrackets) {
+    for (const bracket of insideBrackets) {
+      for (let i = 0; i < bracket.length - 3; i++) {
+        const a = bracket[i];
+        const b = bracket[i + 1];
+        const c = bracket[i + 2];
+        const d = bracket[i + 3];
+        if (a === d && b === c && a !== b) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+async function main() {
+  const file = Bun.file("./input.txt");
+  const data = await file.text();
+
+  const content = data.split("\n").filter(isTLS).length;
+  console.log(content);
+}
+
+await main();
+export {};
